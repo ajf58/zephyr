8fe01a86fb3353f18173698ab986f7db7edca479
Andrew Featherstone <andrew.featherstone@gmail.com>
2024-08-21 14:28:17 +0100

    soc: rp2350: add missing include

---
+++ include/zephyr/dt-bindings/pinctrl/rpi-pico-rp2350-pinctrl.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, Yonatan Schachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __RP2350_PINCTRL_H__
+#define __RP2350_PINCTRL_H__
+
+#include "rpi-pico-rp2040-pinctrl.h"
+
+#endif /* __RP2350_PINCTRL_H__ */
